c1bda1cf4ca72beaf29e73e3f9283be300f0bfa2
esmaeken <140588679+esmaeken@users.noreply.github.com>
2023-07-26 10:28:25 +0300

    Add files via upload

---
+++ Content Csp.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+pd.read_csv('content_consumption_2023.csv')
+
+
+# In[20]:
+
+
+csp = pd.read_csv('content_consumption_2023.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+from datetime import datetime
+
+csp = 'content_consumption_2023.csv'
+
+hour_counts = {}                      #dictionary to analyse csp for every hour 
+
+with open(csp, 'r') as file:          #opening a file in read mode      #with statement to close a file after using
+    reader = csv.reader(file)         
+    next(reader)                       #to skip the header
+    
+    for row in reader:                #loop goes through every line
+        dateandtime = row[6]
+        if dateandtime:
+            dateandtime1 = datetime.strptime(dateandtime, "%Y-%m-%d %H:%M:%S.%f")
+            hour = dateandtime1.hour             #extracting the hours
+            
+            if hour in hour_counts:
+                hour_counts[hour] += 1
+            else:
+                hour_counts[hour] = 1
+
+hours = sorted(hour_counts.keys())
+counts = [hour_counts[hour] for hour in hours]
+
+plt.bar(hours, counts)
+plt.xlabel('Hours of the Day')
+plt.ylabel('Content Consumption Count')
+plt.title('Content Consumption Distribution')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
